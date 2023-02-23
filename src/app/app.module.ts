8c19f7900f66509bcab7580d30970294cfaba44e
Robert Grapffel <grapffel.robert@gmail.com>
2023-02-23 12:46:06 +0200

    Update angular project so it can work on local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+// import { HttpModule } from '@angular/http';
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { MockMemoryDataService } from './mock-memory-data.service';
-import { UserService } from './user.service';
-import { AppRoutingModule } from './app-routing.module';
-import { EasyComponent } from './components/easy/easy.component';
-import { MediumComponent } from './components/medium/medium.component';
-import { ExtremeComponent } from './components/extreme/extreme.component';
-import { HardComponent } from './components/hard/hard.component';
+import { AppComponent } from "./app.component";
+import { MockMemoryDataService } from "./mock-memory-data.service";
+import { UserService } from "./user.service";
+import { AppRoutingModule } from "./app-routing.module";
+import { EasyComponent } from "./components/easy/easy.component";
+import { MediumComponent } from "./components/medium/medium.component";
+import { ExtremeComponent } from "./components/extreme/extreme.component";
+import { HardComponent } from "./components/hard/hard.component";
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    HttpModule,
+    // HttpModule,
     AppRoutingModule,
 
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
