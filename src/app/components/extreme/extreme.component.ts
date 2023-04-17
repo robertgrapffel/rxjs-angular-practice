76103a1a4f899c9301ca22f133e88fa8531620bb
Robert Grapffel <grapffel.robert@gmail.com>
2023-04-17 15:29:41 +0300

    Update extreme

--- src/app/components/extreme/extreme.component.ts
+++ src/app/components/extreme/extreme.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import {
-  AgeType,
   User,
   Age,
   Identity,
   IdentityEnum,
+  UserId,
 } from "src/app/models/user.model";
 import { UserService } from "src/app/services/user.service";
-import { combineLatest, map, take, tap } from "rxjs";
+import { combineLatest, debounceTime, shareReplay, take } from "rxjs";
 
 @Component({
   selector: "app-extreme",
@@ -17,19 +17,54 @@ import { combineLatest, map, take, tap } from "rxjs";
 })
 export class ExtremeComponent implements OnInit {
   public extremeForm: FormGroup;
-  public searchedUser;
-  public foundUsers: User[] | [] = [];
+  public matchedUsers: any = [];
+  public userAges: Age[] | undefined = [];
   public isInBlackList: boolean = false;
   public isDirty = false;
   public userType: string | undefined = "";
 
+  private _allUsers: User[] = [];
+  private _blackList: UserId[] = [];
+
   constructor(private _fb: FormBuilder, private _userService: UserService) {
     this.extremeForm = this._buildForm();
-
-    this.searchedUser = this.extremeForm.get("userInput")?.value;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this._getData();
+
+    this.extremeForm
+      .get("userInput")
+      ?.valueChanges.pipe(debounceTime(500))
+      .subscribe((input) => {
+        this.matchedUsers = this._allUsers.filter((user) =>
+          user.nume.toLowerCase().includes(input)
+        );
+        this._checkBlackList();
+      });
+  }
+
+  public getMoreInfo = (user: any) => {
+    return combineLatest([
+      this._userService.getUsersAgesTypes(),
+      this._userService.getUsersIdentities(),
+    ])
+      .pipe(take(1))
+      .subscribe(([types, identities]) => {
+        const userType = types.find(
+          (type) =>
+            user.age && +type.ageMax >= +user.age && +type.ageMin <= +user.age
+        )?.type;
+        const userIdentity = identities.find(
+          (identity: Identity) => identity.id === user.id
+        );
+
+        const identity =
+          IdentityEnum[userIdentity?.type || IdentityEnum.Personal];
+
+        alert(userType + " " + identity);
+      });
+  };
 
   private _buildForm = () => {
     return this._fb.group({
@@ -37,36 +72,32 @@ export class ExtremeComponent implements OnInit {
     });
   };
 
-  //Stuck here !!!!
-  onInputChange() {
-    this.isDirty = this.extremeForm.get("userInput")?.dirty!;
-    combineLatest([
-      this._userService.getUsers(),
-      this._userService.getBlackList(),
-      this._userService.getUsersAges(),
-      this._userService.getUsersAgesTypes(),
-      this._userService.getUsersIdentities(),
-    ])
-      .pipe(
-        take(1),
-        map(([users, blackList, ages, ageTypes, identities]) => {
-          if (this.isDirty) {
-            this.foundUsers = users.filter((user) =>
-              user.nume.toLowerCase().includes(this.searchedUser)
-            );
-            this.foundUsers.map((user) => {
-              if (blackList.find((blockedUser) => blockedUser.id === user.id)) {
-                this.isInBlackList = true;
-              }
-              let userAge = ages.find((age) =>
-                age ? age.id === user.id : "None"
-              )?.age;
+  private _checkBlackList = () => {
+    this.matchedUsers.map((user: User) => {
+      this.isInBlackList = this._blackList.some(
+        (blockedUser) => blockedUser.id === user.id
+      );
+    });
+  };
 
-              return userAge;
-            });
-          }
-        })
-      )
-      .subscribe((res) => console.log(res));
-  }
+  private _getData = () => {
+    return combineLatest([
+      this._userService.getUsers(),
+      this._userService.getUsersAges(),
+      this._userService.getBlackList(),
+    ])
+      .pipe(take(1), shareReplay())
+      .subscribe(([allUsers, userAges, blackList]) => {
+        allUsers.map((user) => {
+          const userAge = userAges.find((ages) => ages.id === user.id);
+
+          this._blackList = blackList;
+
+          return this._allUsers.push({
+            ...user,
+            ...userAge,
+          });
+        });
+      });
+  };
 }
